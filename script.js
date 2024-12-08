c3f1cc32817e6203ea6f10366435fefcf0e511a7
SebastianHops <h23sebho@du.se>
2024-12-08 11:51:56 +0100

    Final version

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const checkBoxen = document.getElementById ('divStyle'); // hämtar checkbox elementet 
 const remove_Button = document.querySelector('button'); // behövde ändra till button istället för button-1 och queryselector istället för getelementbyclassname
+const inputTextFields = document.querySelectorAll('.textfield'); // variabel tilldelas med querySelectorAll som väljer alla element som matchar med textfield
+const divElement = document.getElementById('div_element'); // variabel som tilldelas med Id selector
 
 const color_Field = document.getElementById('color'); // variabel tilldelas från color input
-const content_Field = document.getElementById('content'); // variabel tilldelas från content input
-const inputTextFields = document.querySelectorAll('.textfield');
-const divElement = document.getElementById('div_element');
+
+console.log(inputTextFields);
+console.log(remove_Button);
+
+
 
 // for each loopar går igenom textfälten med hjälp av variabeln inputTextFields och lägger till en eventlistner för varje element det vill säga 2,
 // blur gör så att när du lämnar en inputruta så blir det en händelse vilket då funktionen handleblur kommer in
@@ -23,43 +27,19 @@ function handleClick() {
 }
 
 // funktionen handle blur skriver ut med hjälp av händelsen blur, input texten i div elemeten
-function handleBlur(e) {
-  const name = e.target.name;
-  const value = e.target.value;
+function handleBlur(event) {
+  const name = event.target.name;
+  const value = event.target.value;
   divElement.innerHTML = '<p> Fältet" ' + name + '" har värdet "' + value + '" </p> ';
+  console.log(event);
 }
 
 // skapar en anonym funktion för checkbox och en eventlistener med händelsen click
 // funktion gör att om checkboxen är ifylld så hämtar den in color input och och sätter divelementets bakgrundsfärg till användarens färgval i input
-checkBoxen.addEventListener('click', function() {
-  if (checkBoxen.checked) {
-    const colorValue = color_Field.value;
+checkBoxen.addEventListener('change', function() {
+  const colorValue = color_Field.value;
     if (colorValue) {
       divElement.style.backgroundColor = colorValue;
     }
   }
-})
-
-
-
-// eventlistener läggs till för att rensa input i textfälten och ta bort div i en anonym funktion
-//remove_Button.addEventListener('click', function() {
-
-  //color_Field.value = ('');
-  //content_Field.value = ('');
-  //divElement.remove();
-//});
-
-/******************************************************************* 
- * // eventlistener läggs till för att rensa input i textfälten och ta bort div i en anonym funktion
-//remove_Button.addEventListener('click', function() {
-
-  //color_Field.value = ('');
-  //content_Field.value = ('');
-  //divElement.remove();
-//});
- * 
-
- ***** anonym funktion bättre för kortare och simplare kod
- ***** icke anonym funktion bättre för återanvänding speciellt för komplexa sidor
-*/
+)
